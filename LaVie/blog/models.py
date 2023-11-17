c0ad76062192646111c7addcd23c1e2a7597360c
Doreen970 <doreenwanyama20@gmail.com>
2023-11-17 23:17:28 +0300

    I wrote some tests for my article model. made modification to the article_list template so that it only displays article title and has a link to detail page when user clicks on it. Made sure that recently published articles are indicated on the list of articles

--- LaVie/blog/models.py
+++ LaVie/blog/models.py
@@ -6,10 +6,12 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+    
 
 #code to show recently published articles
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        
     def __str__(self):
         return self.title
