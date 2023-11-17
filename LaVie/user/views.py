caa96ee5eec0069adcd380f884d5111378a84449
Doreen970 <doreenwanyama20@gmail.com>
2023-11-18 00:30:44 +0300

    created a user app for user accounts. wrote the signup, login and logout views

---
+++ LaVie/user/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')  # Redirect to the home page after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/signup.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')  # Redirect to the home page after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'user/login.html', {'form': form}) 
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')        
