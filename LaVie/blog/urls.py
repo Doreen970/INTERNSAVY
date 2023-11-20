8c0632058b7a6127558dbacb1de5d42607704bf2
Doreen970 <doreenwanyama20@gmail.com>
2023-11-20 10:40:55 +0300

    wrote users authentication. Added signup and login links on the home page for users that are logged out. Restricted the feature of seeing post lists and creation of a blog to only logged in users.

--- LaVie/blog/urls.py
+++ LaVie/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name='index'),
     path('articles/', views.article_list, name='article_list'),
     path('articles/<int:pk>/', views.detail, name='detail'),
     path('create/', views.create_article, name='create_article'),
--- LaVie/blog/views.py
+++ LaVie/blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Article
 from .forms import BlogForm
 
+#@login_required
 def index(request):
-    return HttpResponse("Hello dear, welcome!")
-
+    if request.user.is_authenticated:
+        return render(request, 'blog/index.html', {'user': request.user})
+    else:
+        return render(request, 'blog/index.html')    
+    
+@login_required
 def create_article(request):
     if request.method == 'POST':
         form = BlogForm(request.POST)
@@ -16,10 +21,12 @@ def create_article(request):
         form = BlogForm()
     return render(request, 'blog/create.html', {'form': form})                
 
+@login_required
 def article_list(request):
     articles = Article.objects.all()
     return render(request, 'blog/article_list.html', {'articles': articles})
 
+@login_required
 def detail(request, pk):
     article = Article.objects.get(pk=pk)
     return render(request, 'blog/detail.html', {'article': article})
