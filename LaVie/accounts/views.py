842621c66826868f427ede0bfd2b7c6b23ed9cff
Doreen970 <doreenwanyama20@gmail.com>
2023-12-27 23:17:25 +0300

    created a new branch to come up with a registration form where users put their emails

--- LaVie/accounts/views.py
+++ LaVie/accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignupForm
 
 # Create your views here.
+"""
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
@@ -29,4 +31,20 @@ def user_login(request):
 
 def user_logout(request):
     logout(request)
-    return redirect('index')        
+    return redirect('index') 
+    """  
+def signup(request):
+    form = SignupForm()
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('/login')
+        
+        else:
+            form = SignupForm()
+    return render(request, "accounts/signup.html", {
+        "form": form
+    })         
+
+        
